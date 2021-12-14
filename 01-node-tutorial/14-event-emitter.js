364dcb7d8ce2af24cf5f9e0f6cbeebf26ff23e9f
Barun Debnath <barundebnath91@gmail.com>
2021-12-14 22:50:10 +0530

    events done

---
+++ 01-node-tutorial/14-event-emitter.js
@@ -0,0 +1,16 @@
+const EventEmitter = require("events");
+
+const customEmitter = new EventEmitter();
+
+// on - listen for an event
+// emit - emit an event
+
+customEmitter.on("response", (name, id) => {
+  console.log(`data received user: ${name} with id: ${id}`);
+});
+
+customEmitter.on("response", () => {
+  console.log(`some other logic here`);
+});
+
+customEmitter.emit("response", "john", 34);
